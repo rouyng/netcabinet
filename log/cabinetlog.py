c5cf28be4485a9c6940d7b7cdfd78d2893973be2
rouyng <54250428+rouyng@users.noreply.github.com>
2019-09-02 14:15:04 -0700

    adjust logging script csv output

--- log/cabinetlog.py
+++ log/cabinetlog.py
@@ -7,11 +7,11 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     try:
         ser_bytes = ser.readline()
-        decoded_bytes = str(ser_bytes[0:len(ser_bytes) - 2].decode("utf-8"))
-        today = datetime.date.today()
-        with open('~/{}-cabinetlog.csv'.format(today.strftime("%m.%d.%y")) as f:
-            writer = csv.writer(f, delimiter="")
-            writer.writerow([time.time(), ",", decoded_bytes])
+        decoded_bytes = ser_bytes[0:len(ser_bytes) - 3].decode("utf-8")
+        currtime = datetime.datetime.now()
+        with open('{}-cabinetlog.csv'.format(currtime.strftime("%m.%d.%y")), 'a') as f:
+            writer = csv.writer(f, delimiter=",", escapechar=' ', quoting=csv.QUOTE_NONE)
+            writer.writerow([currtime.strftime("%c"), decoded_bytes])
     except KeyboardInterrupt:
         print('Keyboard interrupt!')
         break
